a410142069d109d0f9eef9f1c4fa314717df5bd0
Wasanta <kwan_u_day@hotmail.com>
2021-11-02 10:03:33 -0400

    commit tree 376e5c7623912aa42b5b3043a4bec82f88a1a775

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 
 // Write your code here
+
+// function multiply(){
+//     let numOne;
+//     let numTwo;
+//     return numOne * numTwo
+// }
+// multiply(31,2);
+
+// function multiply(num1 , num2){
+//     return num1 * num2
+//     }
+// multiply(2,31);
+
+const num1 = 31;
+const num2 = 2
+const multiply = num1*num2
+const random = Math.floor(Math.random()*1000)+1;
+const num3 = 29;
+const num4 = 5
+const mod = num3 % num4
+const max = Math.max(1,3,7,9,20);
+
+
+
